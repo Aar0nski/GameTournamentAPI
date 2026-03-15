9e55fde6cc8e81064fe2e203a89e54db4213b2e2
aar0nski <aaron.enander04@gmail.com>
2026-03-16 00:07:14 +0100

    Completed GameTournament API with CRUD, DTOs, EF Core and validation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using GameTournamentApi.Data;
+using GameTournamentApi.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITournamentService, TournamentService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,4 +29,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-}
